ddb083725d2a9b7c81c4f391fecf96f59bdf1fd3
alahdel <alahdelmohammed@gmail.com>
2022-07-16 16:17:20 +0300

    test

---
+++ resources/views/hallo.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>Laravel</title>
+      <meta charset="utf-8" />
+   </head>
+   <body>
+      <div class = "container">
+        <h2>Hello World!</div>
+      </div>
+   </body>
+</html>
